dfba45a1cca83aa8793fa6bd7e2ef15b9bc3a1d3
Steven Enamakel <enamakel@vezures.com>
2021-02-07 19:22:11 +0300

    Updates

---
+++ precache-manifest.dda342b936a97a14aa6e5c6d425417db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90593e443edcac04357811195d1720c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "737182884c58b5b3de7d",
+    "url": "/static/js/2.b05f4dc2.chunk.js"
+  },
+  {
+    "revision": "48dc2a0f18dfdbbcdcf81b94d8a3e1da",
+    "url": "/static/js/2.b05f4dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac399467c7868b3148d2",
+    "url": "/static/js/main.86b4cf8f.chunk.js"
+  },
+  {
+    "revision": "d7b5075ecf70ba60e7e7",
+    "url": "/static/js/runtime-main.9cb2a799.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
